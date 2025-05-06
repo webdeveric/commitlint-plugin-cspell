6f770435a5cd822597415751f241d9d588941065
Eric King <eric@webdeveric.com>
2025-05-06 10:42:58 -0500

    feat(release): updated `semantic-release` config

--- release.config.mjs
+++ release.config.mjs
@@ -3,4 +3,40 @@
  */
 export default {
   branches: ['main'],
+  preset: 'conventionalcommits',
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [
+          {
+            type: 'chore',
+            scope: 'deps',
+            release: 'minor',
+          },
+          {
+            type: 'chore',
+            scope: 'deps-dev',
+            release: false,
+          },
+          {
+            type: 'docs',
+            release: 'patch',
+          },
+          {
+            type: 'refactor',
+            release: 'patch',
+          },
+          {
+            type: 'chore',
+            scope: 'spelling',
+            release: 'patch',
+          },
+        ],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+  ],
 };
